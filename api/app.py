ea6539c173dd88f607e176b6a6d7df8ae15c763c
Pedro Ramos <pedro.henrique@codeminer42.com>
2022-03-27 11:34:30 -0300

    Add symbols per request restriction

--- api/app.py
+++ api/app.py
@@ -1,17 +1,24 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, make_response, request
 from scrapper import get_stocks_payment_dates
 from formatter import format_payments_to_json
+from errors import OutOfBoundsError
 
 app = Flask(__name__)
 
-@app.route('/payments')
+@app.route('/payments', methods=['GET'])
 def index():
+	symbols = request.args.get('symbols', '').split(',')
+
 	try:
-		symbols = request.args.get('symbols', '').split(',')
+		if len(symbols) > 5:
+			raise OutOfBoundsError
+
 		scrapped_payment_dates = get_stocks_payment_dates(symbols)
 		return format_payments_to_json(scrapped_payment_dates)
+	except OutOfBoundsError as e:
+		return make_response({'error': e.message}, e.status_code)
 	except:
-		return jsonify([])
+		return make_response({'error': 'An unknown error had occurred.'}, 500)
 
 
 if __name__ == '__main__':
