4b62f3449b9ab8c243143f14f4e19f797ababe3d
Pedro Ramos <pedro.henrique@codeminer42.com>
2022-04-02 19:33:20 -0300

    Remove ScrapError

--- api/errors.py
+++ api/errors.py
@@ -1,7 +1,3 @@
 class OutOfBoundsError(Exception):
 	status_code = 400
 	message = 'Only up to 5 symbols can be searched simultaneously.'
-
-class ScrapError(Exception):
-	def __init__(self, symbol):
-		self.message = f'Failed to get scrap data from symbol {symbol}'
